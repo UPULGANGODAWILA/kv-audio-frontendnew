04f0c44f31630f7c03a09127e0e06bb076d120eb
Upul Gangodawila <upulkgangodawila1981@gmail.com>
2025-02-04 19:24:10 +0530

    Day11 Updare

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 
-import AdminPage from "./components/adminPage";
+import AdminPage from "./pages/admin/adminPage";
 import { BrowserRouter,Route,Routes } from "react-router-dom";
+import HomePage from "./pages/home/homePage";
 function App() {
   return (
    
       <BrowserRouter> 
       <Routes path="/*">
-        <Route path="admin" element={<AdminPage/>}/>
-        <Route path="/" element={<h1>home Page</h1>}/>
-        <Route path="/*" element={<h1>Not fount Page Error 500</h1>}/>
+      <Route path="/admin/*" element={<AdminPage/>}/>
+      <Route path="/*" element={<HomePage/>}/>
+        
       </Routes>
-           
+
 
       </BrowserRouter>
    

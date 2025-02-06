0977b07ef64dcb4f85a713ba481103e7a06f1ca1
Upul Gangodawila <upulkgangodawila1981@gmail.com>
2025-02-06 21:30:08 +0530

    login int.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import AdminPage from "./pages/admin/adminPage";
 import { BrowserRouter,Route,Routes } from "react-router-dom";
 import HomePage from "./pages/home/homePage";
+import LoginPage from "./pages/login/login.jsx";
 function App() {
   return (
    
       <BrowserRouter> 
       <Routes path="/*">
       <Route path="/admin/*" element={<AdminPage/>}/>
+      <Route path="/login" element={<LoginPage/>}/>
       <Route path="/*" element={<HomePage/>}/>
         
       </Routes>

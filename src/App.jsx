d84a8dc7389ab81f48008d2555149b261818cb25
Upul Gangodawila <upulkgangodawila1981@gmail.com>
2025-02-12 11:45:25 +0530

    Day 12 👍

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import AdminPage from "./pages/admin/adminPage";
 import { BrowserRouter,Route,Routes } from "react-router-dom";
 import HomePage from "./pages/home/homePage";
 import LoginPage from "./pages/login/login.jsx";
+import { Toaster } from "react-hot-toast";
 function App() {
   return (
    
       <BrowserRouter> 
+       <Toaster position="top-right"/>
       <Routes path="/*">
       <Route path="/admin/*" element={<AdminPage/>}/>
       <Route path="/login" element={<LoginPage/>}/>
